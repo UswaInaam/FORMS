b2a1f300bd42b87f4e000efed17c290f9eab45c6
Uswa Inaam <uswainam267@gmail.com>
2025-11-09 22:34:27 +0500

    forgot password file

--- src/forgot-password.php
+++ src/forgot-password.php
@@ -49,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 $mail->Host = "smtp.gmail.com";
                 $mail->SMTPAuth = true;
                 $mail->Username = "uswainam267@gmail.com";
-                $mail->Password = "umqn kpgz zxgm mdep";
+                $mail->Password = "";
                 $mail->SMTPSecure = "tls";
                 $mail->Port = 587;
                 $mail->setFrom("uswainam267@gmail.com");
